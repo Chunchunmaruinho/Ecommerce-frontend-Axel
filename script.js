8d767546a400160992f9a54131418b16b8b380eb
Chunchunmaruinho <axelprogaymer33d@gmail.com>
2023-09-01 12:55:13 -0300

    feat(): update

---
+++ script.js
@@ -0,0 +1,13 @@
+const PRODUCTO_DESTACADO = {
+    imgProducto:  '1',
+    cardTitle: 'Pala' ,
+    identifier: '111',
+    prices: {    
+        oldPrice: '$10',
+        newPrice: '$10'
+    }
+}
+
+window.onload = function() {
+    
+}
