2e2f26eba9a93ccb51d3e1e87a98198ee5d67bc9
Chunchunmaruinho <axelprogaymer33d@gmail.com>
2023-09-06 13:09:06 -0300

    feat(): API

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-function createCard(PRODUCTO_DESTACADO) {
+
+async function fetchProducts() {
+  console.log("Init");
+
+  const apiURL = "https://64f659ae2b07270f705e6753.mockapi.io/api/products";
+  const apiResponse = await fetch(apiURL)
+  const jsonData = await apiResponse.json();
+  console.log(apiResponse.status);
+
+  console.log(jsonData);
+  return jsonData;
+}
+
+
+
+
+function createCard(product) {
     const {
-    imgProducto =  'images/productos/DestacadosDesktop.png',
+    image =  'images/productos/DestacadosDesktop.png',
     desProducto ='Alt producto' ,
-    cardTitle = 'Nombre del producto' ,
+    title = 'Nombre del producto' ,
     identifier = 'identificador',    
-    oldPrice = '$Precio Antiguo',
-    newPrice = '$Precio Actual'   
-    } = PRODUCTO_DESTACADO;
+    previousPrice = '$Precio Antiguo',
+    price = '$Precio Actual'   
+    } = product;
 
     const card = document.createElement('div');
     card.className = 'card'; 
@@ -19,13 +35,13 @@ function createCard(PRODUCTO_DESTACADO) {
 
     const imgProductoI = document.createElement('img');
     imgProductoI.className =  'imgProducto';  
-    imgProductoI.src = imgProducto; 
-    imgProducto.alt = desProducto; 
+    imgProductoI.src = image; 
+    image.alt = desProducto; 
     
 
     const cardTitleH = document.createElement('h3');
     cardTitleH.className = 'card-title';
-    cardTitleH.textContent = cardTitle;
+    cardTitleH.textContent = title;
    
 
     const identifierP = document.createElement('p');
@@ -35,7 +51,7 @@ function createCard(PRODUCTO_DESTACADO) {
 
     const oldPriceP = document.createElement('p');
     oldPriceP.className = 'old-price';
-    oldPriceP.textContent = oldPrice;
+    oldPriceP.textContent = previousPrice;
 
     const cardOrder = document.createElement('div');
     cardOrder.className = 'card-order';
@@ -71,7 +87,7 @@ function createCard(PRODUCTO_DESTACADO) {
 
     const newPriceP = document.createElement('p');
     newPriceP.className = 'new-price';
-    newPriceP.textContent = newPrice;
+    newPriceP.textContent = price;
 
 
     cardButtom.appendChild(imgBag);
@@ -81,42 +97,18 @@ function createCard(PRODUCTO_DESTACADO) {
 
     return card;
 }
-
-const cardData = [
-    {
-      imgProducto: 'images/productos/DestacadosDesktop.png',
-      desProducto: 'This is the content of Card 1.',
-      cardTitle: 'Titulo del primer producto',
-      identifier:'111',
-      oldPrice:'$10',
-      newPrice:'$234',
-    },
-    {
-        imgProducto: 'images/productos/DestacadosDesktop.png',
-        desProducto: 'This is the content of Card 1.',
-        identifier:'111',
-        oldPrice:'$10',
-        newPrice:'$23423',
-      },
-      {
-        imgProducto: 'images/productos/DestacadosDesktop.png',
-        desProducto: 'This is the content of Card 1.',
-        cardTitle: 'Titulo del tercer producto',
-        identifier:'111',
-        oldPrice:'$10',
-        newPrice:'$234',
-      },
-  ];
   
 
 let cardContainer;
 
-window.addEventListener("load", (event) => {
+window.addEventListener("load", async (event) => {
     console.log("page is fully loaded");
+
+    const products = await fetchProducts();
 
     cardContainer = document.getElementById('products');
 
-    for (const data of cardData) {
+    for (const data of products) {
         const card = createCard(data);
         cardContainer.appendChild(card);
       }
